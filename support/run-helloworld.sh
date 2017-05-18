53f48aa03b864da9e731513ed9aa8e283ce5fae6
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2017-05-18 09:26:18 +0100

    Update run-helloworld.sh

--- support/run-helloworld.sh
+++ support/run-helloworld.sh
@@ -1,5 +1,7 @@
 
 # New TensorFlow 
+echo " $# javac -cp libtensorflow-1.1.0.jar HelloTF.java "
 echo " $# java -cp libtensorflow-1.1.0.jar:. -Djava.library.path=./jni HelloTF "
+javac -cp libtensorflow-1.1.0.jar HelloTF.java
 java -cp libtensorflow-1.1.0.jar:. -Djava.library.path=./jni HelloTF
 
